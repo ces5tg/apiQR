bbb0c17d0859b9c7c06b1974e015ab88ec6abbdd
dennys3mf <dennys.marquez@tecsup.edu.pe>
2023-12-03 18:56:29 -0500

    esquema_personas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar.component";
+import { UserProvider } from "./contexts/user.context";
+import Analytics from "./pages/Analytics.page";
+import CreateExpense from "./pages/CreateExpense.page";
+import EditExpense from "./pages/EditExpense.page";
+import Home from "./pages/Home.page";
+import Login from "./pages/Login.page";
+import PrivateRoute from "./pages/PrivateRoute.page";
+import Signup from "./pages/Signup.page";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      {/* We are wrapping our whole app with UserProvider so that */}
+      {/* our user is accessible through out the app from any page*/}
+      <UserProvider>
+        <NavBar />
+        <Routes>
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<Signup />} />
+          {/* We are protecting our Home Page from unauthenticated */}
+          {/* users by wrapping it with PrivateRoute here. */}
+          <Route element={<PrivateRoute />}>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/new" element={<CreateExpense />} />
+            <Route exact path="/expense/:id/edit" element={<EditExpense />} />
+            <Route exact path="/analytics" element={<Analytics />} />
+          </Route>
+        </Routes>
+      </UserProvider>
+    </BrowserRouter>
   );
 }
 
---
+++ src/realm/constants.js
@@ -0,0 +1,2 @@
+export const APP_ID = "application-0-zuijq";
+export const GRAPHQL_ENDPOINT = "https://us-east-1.aws.realm.mongodb.com/api/client/v2.0/app/application-0-zuijq/graphql"
